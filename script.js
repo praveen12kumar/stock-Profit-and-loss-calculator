695c08f30138b49daaf114fd61c87985d33d43cc
praveen kumar <praveenshakya1@gmail.com>
2023-01-11 19:43:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let outputDiv = document.querySelector(".output-stock");
 
 // add addeventListner
 TellMe.addEventListener("click", function(){
-    let ans = calculateProfitAndLoss(initialPrice.value, stockNumber.value, currentPrice.value)
+    calculateProfitAndLoss(initialPrice.value, stockNumber.value, currentPrice.value)
 })
 
 // function
@@ -17,24 +17,21 @@ TellMe.addEventListener("click", function(){
 
 
 function calculateProfitAndLoss(initial, quantity, current){
-    let loss, lossPercentage, profit, profitPercentage;
-    //
     if(initial > current){
-        loss = (initial - current);
-        lossPercentage = (loss / initial) * 100;
+        var loss = (initial - current)*quantity;
+        var lossPercentage = (loss / initial) * 100;
         outputDiv.style.color = "red";
-        showOutput(`Hey the loss is ${loss} and the loss percent is ${lossPercentage}`);
+        showOutput(`Hey the loss is ${loss} and the loss percent is ${lossPercentage}%`);
     }
-
     else if(current > initial){
-        profit = (current - initial);
-        profitPercentage = (profit / initial) * 100;
+        var profit = (current - initial)*quantity;
+        var profitPercentage = (profit / initial) * 100;
         outputDiv.style.color = "green";
-        showOutput(`Hey the Profit is ${profit} and the profit percent is ${profitPercentage}`);
+        showOutput(`Hey the Profit is ${profit} and the profit percent is ${profitPercentage}%`);
     }
     else{
-        
-        showOutput('No pain no gain and no gain no pain')
+        outputDiv.style.color = "white";
+        showOutput(`No pain no gain and no gain no pain`)
     }
 }
 
